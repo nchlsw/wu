84c559397ddd83fc79d31a8c18defcfadcea3258
nchlsw <nichowuu@gmail.com>
2022-02-10 15:42:22 +0700

    exercise : .indexOf duplicate

---
+++ index.js
@@ -0,0 +1,30 @@
+// Duplikasi method .find
+// Duplikasi method .indexOf
+
+// Conditions (indexOf)
+// 1. Mencari index dari sebuah item dalam array
+// 2. Akan return sebuah number (index)
+
+// Conditions (find)
+// 1. Mencari sebuah value (item) dalam array yang
+//    memenuhi condition yang kita berikan
+// 2. Callback function kita harus return boolean
+// 3. Ketika callback function return true, loop akan berhenti
+//    lalu function find akan return item dari array.
+
+// .indexOf
+
+// let index = books.indexOf("LOTR");
+// console.log(index);
+
+const books = ["LOTR", "Harry Potter", "Sherlock", "Naruto"];
+const indexOfDuplicate = (books, val) => {
+  for (let i = 0; i < books.length; i++) {
+    if (books[i] == val) {
+      return i;
+    }
+  }
+};
+let duplicateIndexOf = indexOfDuplicate(books, "Harry Potter");
+
+console.log(duplicateIndexOf);
